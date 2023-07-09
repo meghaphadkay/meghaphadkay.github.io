5f98469aa13fdc05f0efd9f71aedd5f3f4196e3f
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2023-07-09 10:46:17 +0530

    More animations

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,11 +1,17 @@
 const openNavButton = document.querySelector("#open-nav-button");
 const closeNavButton = document.querySelector("#close-nav-button");
 const mobileNavbar = document.querySelector("#mobile-nav");
+const navSocialLink = document.querySelector("#mobile-nav .nav-social-links");
+const navPageLinks = document.querySelectorAll("#mobile-nav .nav-link");
 
 openNavButton.addEventListener('click', () => {
     mobileNavbar.style.left = '0';
+    navSocialLink.classList.add('slide-up');
+    navPageLinks.forEach(link => {link.classList.add('fade-in')});
 })
 
 closeNavButton.addEventListener('click', () => {
     mobileNavbar.removeAttribute('style');
+    navSocialLink.classList.remove('slide-up');
+    navPageLinks.forEach(link => {link.classList.remove('fade-in')});
 })
