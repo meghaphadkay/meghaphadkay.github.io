cc33b5332bab767fbbe3e22edc4050579c45a520
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2023-07-25 08:19:51 +0530

    Added awards page

---
+++ static/js/awards-renderer.js
@@ -0,0 +1,44 @@
+const awardsContainer = document.querySelector('#awards-container');
+const awardsText = document.querySelector('#awards-text').src;
+
+const awards = [];
+
+fetch(awardsText)
+    .then(response => response.text())
+    .then(text => {
+        text = text.split('\n');
+        let description = '';
+        let currentAward = {};
+        let currAwardName;
+
+        for (let t of text) {
+            if (t.toLowerCase().startsWith("- award")) {
+                currentAward.description = description;
+                if (currAwardName) {
+                    currentAward.name = currAwardName;
+                    awards.push(currentAward);
+                }
+                description = '';
+                currAwardName = splitOnce(t, ":");
+                currentAward = {};
+            }
+            else if (t.toLowerCase().startsWith("- date")) currentAward.date = splitOnce(t, ":");
+            else description += t.trim() + ' ';
+        }
+        currentAward.description = description;
+        currentAward.name = currAwardName;
+        awards.push(currentAward);
+    })
+    .then(() => {
+        for (let award of awards) {
+            const awardCard = document.createElement('div');
+            awardCard.classList.add('flexbox-column', 'column-half', 'pad-30', 'award-container');
+            awardCard.innerHTML = `
+                <h2>${award.name}</h2>
+                <span class="award-date"><i>${award.date}</i></span>
+                <p>${award.description}</p>
+            `;
+            awardsContainer.appendChild(awardCard);
+        }
+        awardsContainer.classList.add('slide-up');
+    })
